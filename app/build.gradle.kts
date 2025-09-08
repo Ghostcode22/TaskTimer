ee66fbef1787364eb48adde6bda07073fef497d6
Ghostcode22 <134975339+Ghostcode22@users.noreply.github.com>
2025-09-07 19:56:51 -0500

    Basic UI created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,8 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
